bb060ba7d3add099fc4a756addea186ba25b55e4
sru <simon.rutz@outlook.de>
2024-06-24 11:01:51 +0200

    add crossorigin

--- src/main/java/ch/compsteel/componentSupplier/Controller/ComponentSupplierController.java
+++ src/main/java/ch/compsteel/componentSupplier/Controller/ComponentSupplierController.java
@@ -3,6 +3,7 @@ package ch.compsteel.componentSupplier.Controller;
 import ch.compsteel.componentSupplier.Domain.Data;
 import ch.compsteel.componentSupplier.Domain.DataRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,12 @@ public class ComponentSupplierController {
 
     private DataRepository dataRepository;
 
+    @CrossOrigin(origins = "localhost:4321")
     @PostMapping("/extremelySecureDefinitelyNotMaliciousUploadOfDataThisEndpointIsOnlyToEnsureSecurity")
     public ResponseEntity<Map<String, Object>> uploadComponent(@RequestBody Map<String, Object> json) {
         System.out.println("Received JSON: " + json);
 
+        System.out.println(json);
         dataRepository.save(new Data(json));
 
         return new ResponseEntity<>(json, HttpStatus.OK);

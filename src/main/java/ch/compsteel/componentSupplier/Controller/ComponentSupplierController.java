9a2f510f3b4d41cb265951be138481e20ebb081b
sru <simon.rutz@outlook.de>
2024-07-01 18:37:18 +0200

    allow cors and add docs folder with showcase videos

--- src/main/java/ch/compsteel/componentSupplier/Controller/ComponentSupplierController.java
+++ src/main/java/ch/compsteel/componentSupplier/Controller/ComponentSupplierController.java
@@ -3,11 +3,11 @@ package ch.compsteel.componentSupplier.Controller;
 import ch.compsteel.componentSupplier.Domain.Data;
 import ch.compsteel.componentSupplier.Domain.DataRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,7 +17,7 @@ public class ComponentSupplierController {
 
     private DataRepository dataRepository;
 
-    @CrossOrigin(origins = "localhost:4321")
+    @CrossOrigin(origins = "http://localhost:4321")
     @PostMapping("/extremelySecureDefinitelyNotMaliciousUploadOfDataThisEndpointIsOnlyToEnsureSecurity")
     public ResponseEntity<Map<String, Object>> uploadComponent(@RequestBody Map<String, Object> json) {
         System.out.println("Received JSON: " + json);
@@ -26,5 +26,12 @@ public class ComponentSupplierController {
         dataRepository.save(new Data(json));
 
         return new ResponseEntity<>(json, HttpStatus.OK);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4321")
+    @GetMapping
+    public ResponseEntity<List<Data>> getComponents() {
+
+        return new ResponseEntity<>(dataRepository.findAll(), HttpStatus.OK);
     }
 }
